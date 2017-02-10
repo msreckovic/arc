0cead909cc7548923d028ad548c5b047536f13fa
Milan Sreckovic <msreckovic@yahoo.com>
2017-02-09 20:56:36 -0500

    Owing with hover tooltip

--- owing.js
+++ owing.js
@@ -9,6 +9,7 @@ function ForTickets(entry, h3s, h3e)
     var amount = entry[1];
     var paid = entry[2];
     var owing = entry[3];
+    var arrears = entry[4];
 
     var summary = "";
     if (amount) {
@@ -16,7 +17,11 @@ function ForTickets(entry, h3s, h3e)
 	summary += "    <td data-label=Who>" + h3s + who + h3e + "</td>\n";
 	summary += "    <td data-label=Amount>" + h3s + amount + h3e + "</td>\n";
 	summary += "    <td data-label=Paid>" + h3s + paid + h3e + "</td>\n";
-	summary += "    <td data-label=Owing>" + h3s + owing + h3e + "</td>\n";
+	summary += "    <td data-label=Owing>" + h3s + owing + h3e;
+	if (arrears) {
+	    summary += "<span class=\"owingtooltip\">" + who + ": " + arrears + "</span>";
+	}
+	summary += "</td>\n";
 	summary += "</tr>";
     }
     return summary;
@@ -34,23 +39,24 @@ function GetOwing(entries, full)
 {
     var people = [];
 
-    var i, owing, who, amount, paid;
+    var i, owing, who, amount, paid, arrears;
     for (i=1; i<entries.length; i++) {
 	who = GetValue(entries[i], "gsx$who", "");
 	owing = GetValue(entries[i], "gsx$owing", "");
 	//		if (owing != 0) {
 	// console.log("This is owing " + owing);
-	if (full || (owing > 2 || owing < -2)) {
+	if (full || owing > 2) {
 	    amount = GetValue(entries[i], "gsx$total", "");
 	    paid = GetValue(entries[i], "gsx$paid", "");
-	    people.push([who, amount, paid, "$"+owing]);
+	    arrears = GetValue(entries[i], "gsx$arrears", "");
+	    people.push([who, amount, paid, "$"+owing, arrears]);
 	}
     }
     people = people.sort(); // Probably don't need to, but OK
 
     var tickets = "<table>\n";
     tickets += "  <thead>\n";
-    tickets += ForTickets(["MEMBER", "REGATTA FEES", "PAID TO DATE", "FEES OUTSTANDING"], "<h3>", "</h3>");
+    tickets += ForTickets(["MEMBER", "REGATTA FEES", "PAID TO DATE", "FEES OUTSTANDING", ""], "<h3>", "</h3>");
     tickets += "  </thead>\n";
 
     tickets += "  <tbody>\n";
